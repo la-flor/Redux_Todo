12e124149b0775101bc49f007acaef75a37f1389
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-22 15:40:41 -0700

    Created Redux instantiation and basic app components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./rootReducer";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider >,
   document.getElementById('root')
 );
 
